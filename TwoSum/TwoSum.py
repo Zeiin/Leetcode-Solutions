55cdab11e751f5de39f98db1df2e6b4104546e39
Zein <hualzein@gmail.com>
2020-08-30 07:59:08 -0400

    Added generic python3 solution

--- TwoSum/TwoSum.py
+++ TwoSum/TwoSum.py
@@ -1,3 +1,4 @@
+#Beats 98.3% of Python3 solution for Memory Usage  
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         compliments = []
@@ -6,3 +7,13 @@ class Solution:
                 return [compliments.index(val), idx]
             else:
                 compliments.append(target-val)
+                
+#Average solution, nothing really special, but 10x faster than the memory-efficiency solution above.                
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        compliments = dict()
+        for idx, val in enumerate(nums):
+            if compliments.get(val, None) != None:
+                return [compliments.get(val), idx]
+            else:
+                compliments[target-val] = idx
